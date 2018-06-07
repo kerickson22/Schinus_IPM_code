dd5162060278f6c4fd9f1a43cd67039c0832cd6d
kerickson22 <kerickson22@gmail.com>
2018-06-07 14:26:45 -0500

    Merge survival models for overall and by biotype

--- demography_overall.R
+++ demography_overall.R
@@ -1,50 +1,73 @@
-#####Code for performing demographic analysis 
+#####Code for performing demographic analysis for overall model as well as by biotype
+#####This code initializes a vector (called p.vec) that will be used in constructed the integral projection models
 ### By Kelley D. Erickson and Carol C. Horvitz
 
-##Set working directory to load data: 
+##### Load packages
+library(RColorBrewer)
+
+
+##### Set working directory to load in data: 
 #MAC
 setwd("/Users/curculion/Dropbox/March 2016 Documents/Documents/Grad/dissertation/Pratt_demographic_data") 
-#WINDOWS:
-setwd("C:/Users/KE2/Dropbox/March 2016 Documents/Documents/Grad/dissertation/Pratt_demographic_data") 
-
-
-load("demography_overall.RData")
-
-
+load("demography_overall.RData") #This RData file was created after running everything in this script
 demog<-read.csv("demography_15_clean.csv", head=T)
 
 
-Death_Cause<-c(demog$Death_Cause_2, demog$Death_Cause_3, demog$Death_Cause_4, demog$Death_Cause_5, demog$Death_Cause_6, demog$Death_Cause_7)
+##### Set colors for plotting different biotypes 
 
+col_E_light<-colors[1]
+col_E_dark<-colors[2]
+
+col_W_light<-colors[5]
+col_W_dark<-colors[6]
+
+col_H_light<-colors[9]
+col_H_dark<-colors[10]
+
+##### Initialize pvec's to store parameters that will go into the IPMs:
+#Initialize p.vec's to store output from model (these will feed into the IPM)
+p.vec_E<-rep(0, 39);
+p.vec_H<-rep(0, 39);
+p.vec_W<-rep(0, 39);
+p.vec_overal<-rep(0, 39)
+
+#### Restructure the data in a few different ways: 
+
+Death_Cause<-c(demog$Death_Cause_2, demog$Death_Cause_3, demog$Death_Cause_4, demog$Death_Cause_5, demog$Death_Cause_6, demog$Death_Cause_7)
 
 Status<-c(demog$Status_1, demog$Status_2, demog$Status_3, demog$Status_4, demog$Status_5, demog$Status_6, demog$Status_7)
 
 Diams<-cbind(demog$Diam_1, demog$Diam_2, demog$Diam_3, demog$Diam_4, demog$Diam_5, demog$Diam_6, demog$Diam_7) 
+
 row.names(Diams)<-demog$Plant_ID
 
 Heights<-cbind(demog$Height_1, demog$Height_2, demog$Height_3, demog$Height_4, demog$Height_5, demog$Height_6, demog$Height_7)
+
 row.names(Heights)<-demog$Plant_ID
 
 Mins<-cbind(demog$Min_1, demog$Min_2, demog$Min_3, demog$Min_4, demog$Min_5, demog$Min_6, demog$Min_7)
+
 row.names(Mins)<-demog$Plant_ID
 
 Maxs<-cbind(demog$Max_1, demog$Max_2, demog$Max_3, demog$Max_4, demog$Max_5, demog$Max_6, demog$Max_7)
-row.names(Maxs)<-demog$Plant_ID
 
+row.names(Maxs)<-demog$Plant_ID
 
 Diams_pooled<-rbind( cbind(Diams[,1], Diams[,2]), cbind(Diams[,2], Diams[,3]), cbind(Diams[, 3], Diams[,4]), cbind(Diams[,4], Diams[,5]), cbind(Diams[,5], Diams[,6]), cbind(Diams[, 6], Diams[, 7]))
 
-
 Diams_pooled<-data.frame(Diams_pooled[,1], Diams_pooled[,2])
+
 names(Diams_pooled)<-c("t", "t_plus_one")
 
-
 Sites_pooled<-rep(demog$Site, 6)
+
 ID_pooled<-rep(demog$Plant_ID, 6)
+
 Location<-rep(demog$Location, 6)
 
 Genetic_type_pooled<-rep(0, length(Sites_pooled))
 
+#code which sites are which biotype
 for (i in 1:length(Genetic_type_pooled)) { 
 	if (Sites_pooled[i]=="Big Cypress") Genetic_type_pooled[i]<-"hybrid"
 	else if (Sites_pooled[i]=="Cape Canaveral") Genetic_type_pooled[i]<-"hybrid"
@@ -82,16 +105,7 @@ Mins_pooled<-rbind( cbind(Mins[,1], Mins[,2]), cbind(Mins[,2], Mins[,3]),
 Mins_pooled<-data.frame(Mins_pooled[,1], Mins_pooled[,2])
 names(Mins_pooled)<-c("t", "t_plus_one")
 
-
-
-
-
-
-
-
-
-
-#Max and Min are highly correlated with eachother
+#Max and Min are highly correlated with eachother and should not both be used to describe population dynamics
 
 
 ###Use status matrix to get survival matrix
@@ -101,7 +115,6 @@ survmat<- ifelse(survmat=="prestudy"|survmat=="missing"|survmat=="still_dead", N
 survmat<- ifelse(survmat =="tagged"|survmat=="alive", 1, 0)
 
 survmat_pooled<-rbind( cbind(survmat[,1], survmat[,2]), cbind(survmat[,2], survmat[,3]), cbind(survmat[, 3], survmat[,4]), cbind(survmat[,4], survmat[,5]), cbind(survmat[,5], survmat[,6]), cbind(survmat[, 6], survmat[, 7]))
-
 names(survmat_pooled)<-c("surv_t", "surv_t_plus_one")
 
 
@@ -110,18 +123,14 @@ names(survmat_pooled_bysize)<-c("size_t", "surv_t_plus_one")
 
 
 
-###Use reproduction status matrix to get reproduction matrix
+#Use reproduction status matrix to get reproduction matrix
 repstatmat<-as.matrix(demog[, c(8, 16, 24, 32, 40, 48, 56)])
 repmat<-repstatmat
 repmat<- ifelse(repmat =="female", 1, 0)
 repmat_pooled<-rbind( cbind(repmat[,1], repmat[,2]), cbind(repmat[,2], repmat[,3]), cbind(repmat[, 3], repmat[,4]), cbind(repmat[,4], repmat[,5]), cbind(repmat[,5], repmat[,6]), cbind(repmat[, 6], repmat[, 7]))
 
 
-
 pooled<-cbind(ID_pooled, Sites_pooled, Location, Death_Cause, Genetic_type_pooled, Mins_pooled, Maxs_pooled, Diams_pooled, Heights_pooled, repmat_pooled, survmat_pooled)
-
-
-
 names(pooled)<-c("ID", "Site","Location", "Death_Cause", "Genetic_type", "Min_t", "Min_tplus1", "Max_t", "Max_tplus1", "Diameter_t", "Diameter_tplus1", "Height_t", "Height_tplus1", "Rep_t", "Rep_tplus1", "Surv_t", "Surv_tplus1")
 
 predictors<-cbind(pooled$Min_t, pooled$Diameter_t, pooled$Height_t, pooled$Max_t)
@@ -132,8 +141,6 @@ rownames(correlation)<-c("Min", "Diameter", "Height", "Max")
 
 correlation
 #Over the entire dataset, there is the lowest correlation between diameter and height
-#(In lab group we were looking at correlation matrix for census 1 only )
-write.csv(correlation, file="correlation_matrix_size.csv")
 
 
 #Boxplot of sizes of individuals that survive to t plus 1 and those that do not
@@ -190,11 +197,7 @@ for (i in 1:9072) {
 pooled2<-cbind(pooled, Diam_cat_t, Height_cat_t, Min_cat_t)
 
 
-
-
-
-
-
+#####Cleaning up the data
 
 #Remove individual deaths that were sprayed or fire or grinding
 restrict<-subset(pooled2, is.na(Death_Cause))
@@ -214,18 +217,6 @@ min_table<-table(restrict2$Surv_tplus1, restrict2$Min_cat_t)
 surv_by_min<-min_table[2,]/(min_table[1,] + min_table[2,])
 
 
-
-
-
-
-write.csv(height_table, file="height_table.csv")
-write.csv(min_table, file="min_table.csv")
-write.csv(diam_table, file="diam_table.csv")
-
-write.csv(surv_by_diam, file="surv_by_diam.csv")
-write.csv(surv_by_height, file="surv_by_height.csv")
-write.csv(surv_by_min, file="surv_by_min.csv")
-
 #Divide dataset into size domains (seedlings and larges)
 seedlings<-subset(restrict2, restrict2$Diameter_t<1.6)
 seedlings<-subset(seedlings, seedlings$Height_t<16)
@@ -239,106 +230,90 @@ x1seq_seedlings<-seq(0, 1.6, length.out=50)
 x2seq_seedlings<-seq(0, 16, length.out=50)
 
 
+##### Modeling Survival 
 
-#Create a function for plotting logistic equations (TODO: Do I actually use this function?)
-plot_logistic=function(model, xlab, ylab, x1seq, x2seq){
-b0<-model$coeff[1]
-b1<-model$coeff[2]
-b2<-model$coeff[3]
+#(a) Of D1-sized individuals (overall model; not separated by biotype) 
 
-
-z1<-outer(x1seq, x2seq, function(a,b) ((exp(b0+b1*a +b2*b)/(1+exp(b0+b1*a +b2*b)))))
-
-
-nrz<-nrow(z1)
-ncz<-ncol(z1)
-nbcol<-100
-jet.colors<-colorRampPalette(c("blue", "green"))
-color<-jet.colors(nbcol)
-zfacet<-z1[-1, -1] + z1[-1, -ncz] + z1[-nrz, -1] + z1[-nrz, -ncz]
-facetcol<-cut(zfacet, nbcol)
-
-
-col = color[facetcol]
-
-x11()
-persp(x1seq, x2seq, z1, ticktype="detailed", theta=-30, zlim=c(0, 1.25), zlab="\n P(Survival)", shade=0.55, nticks=4, xlab=xlab, ylab=ylab, border=NA, col = color[facetcol])
-}
-
-
-
-
-#Predicting survival as a function of diameter and growth (removed outliers with large diameters, as well as "unnatural" death, ie fire and spraying)
-# I do not have enough data for an interaction term 
-
-#Full model without interaction:
-
-#Diameter and Height
-
-#Model survival of seedlings in the D1 domain(not separated by biotype)
 surv_mod_seedlings<-glm(seedlings$Surv_tplus1~ seedlings$Diameter_t + seedlings$Height_t, family=binomial)
 
 summary(surv_mod_seedlings)
 #All terms are significant
 
+p.vec_overall[20]<-surv_mod_seedlings$coeff[1]
+p.vec_overall[21]<-surv_mod_seedlings$coeff[2]
+p.vec_overall[22]<-surv_mod_seedlings$coeff[3]
 
-b0<-surv_mod_seedlings$coeff[1]
-b1<-surv_mod_seedlings$coeff[2]
-b2<-surv_mod_seedlings$coeff[3]
+#(b) of D1-sized individuals (by biotype)
 
+surv_mod_seedlings_site<-glm(seedlings$Surv_tplus1~ seedlings$Diameter_t + seedlings$Height_t+seedlings$Site, family=binomial)
+summary(surv_mod_seedlings_site)
+#Because I have low sample size at some sites, I decided to group by biotype instead of focusing on differences between sites.
 
-z1<-outer(x1seq_seedlings, x2seq_seedlings, function(a,b) ((exp(b0+b1*a +b2*b)/(1+exp(b0+b1*a +b2*b)))))
+surv_mod_seedlings_lineage<-glm(seedlings$Surv_tplus1 ~ seedlings$Diameter_t + seedlings$Height_t + seedlings$Genetic_type, family=binomial)
+summary(surv_mod_seedlings_lineage)
+#Hybrid is different from E+W, but Eastern and Western are not different from eachother
 
+#Create a new dataframe where Eastern and Western biotypes are lumped together 
+lineage2<-rep(0, length(seedlings$Genetic_type))
+for (i in 1:length(seedlings$Genetic_type)) {
+  if (seedlings$Genetic_type[i]=="western" || seedlings$Genetic_type[i]=="eastern") {lineage2[i]<-"E-W"}
+  else{lineage2[i]<-"Hybrid"}
+}
 
-nrz<-nrow(z1)
-ncz<-ncol(z1)
-nbcol<-100
-jet.colors<-colorRampPalette(c("blue", "green"))
-color<-jet.colors(nbcol)
-zfacet<-z1[-1, -1] + z1[-1, -ncz] + z1[-nrz, -1] + z1[-nrz, -ncz]
-facetcol<-cut(zfacet, nbcol)
+seedlings2<-cbind(seedlings, lineage2)
 
+surv_mod_seedlings_lineage2<-glm(seedlings2$Surv_tplus1 ~ seedlings2$Diameter_t + seedlings2$Height_t + seedlings2$lineage2, family=binomial)
+summary(surv_mod_seedlings_lineage2)
 
-col = color[facetcol]
+p.vec_E[20]<-surv_mod_seedlings_lineage2$coeff[1]
+p.vec_E[21]<-surv_mod_seedlings_lineage2$coeff[2]
+p.vec_E[22]<-surv_mod_seedlings_lineage2$coeff[3]
 
+p.vec_H[20]<-surv_mod_seedlings_lineage2$coeff[1]+surv_mod_seedlings_lineage2$coeff[4]
+p.vec_H[21]<-surv_mod_seedlings_lineage2$coeff[2]
+p.vec_H[22]<-surv_mod_seedlings_lineage2$coeff[3]
 
+p.vec_W[20]<-surv_mod_seedlings_lineage2$coeff[1]
+p.vec_W[21]<-surv_mod_seedlings_lineage2$coeff[2]
+p.vec_W[22]<-surv_mod_seedlings_lineage2$coeff[3]
 
-setwd("/Users/curculion/Dropbox/UM_Dissertation_LaTeX-master/untitled folder/Figures/New_July")
-png(file="seedling_surv_overall.png", width=10, height=10, units="in", res=300)
-par(ps=24)
-persp(x1seq_seedlings, x2seq_seedlings, z1, ticktype="detailed", theta=-30, zlim=c(0, 1.25), zlab="\n P(Survival)", shade=0.55, nticks=4, xlab="\n Diameter (mm)", ylab="\n \n Height (cm)", col = color[facetcol])
-dev.off()
+#(c) Model D2-survival (overall)
 
-
-#Model D2 survival (not by biotype)
 surv_mod_larges<-glm(larges$Surv_tplus1~larges$Diameter_t + larges$Height_t, family=binomial)
 summary(surv_mod_larges)
 
-b0<-surv_mod_larges$coeff[1]
-b1<-surv_mod_larges$coeff[2]
-b2<-surv_mod_larges$coeff[3]
+p.vec_overall[1]<-surv_mod_larges$coeff[1]
+p.vec_overall[2]<-surv_mod_larges$coeff[2]
+p.vec_overall[3]<-surv_mod_larges$coeff[3]
 
+#(d) Model D2-survival (by biotype)
 
-z1<-outer(x1seq_larges, x2seq_larges, function(a,b) ((exp(b0+b1*a +b2*b)/(1+exp(b0+b1*a +b2*b)))))
+surv_mod_larges_lineage<-glm(larges$Surv_tplus1~larges$Diameter_t + larges$Height_t +larges$Genetic_type, family=binomial)
+summary(surv_mod_larges_lineage)
+#Hybrid is different from E+W, but Eastern and Western are not different from eachother
 
+lineage3<-rep(0, length(larges$Genetic_type))
+for (i in 1:length(larges$Genetic_type)) {
+  if (larges$Genetic_type[i]=="western" || larges$Genetic_type[i]=="eastern") {lineage3[i]<-"E-W"}
+  else{lineage3[i]<-"Hybrid"}
+}
 
-nrz<-nrow(z1)
-ncz<-ncol(z1)
-nbcol<-100
-jet.colors<-colorRampPalette(c("blue", "green"))
-color<-jet.colors(nbcol)
-zfacet<-z1[-1, -1] + z1[-1, -ncz] + z1[-nrz, -1] + z1[-nrz, -ncz]
-facetcol<-cut(zfacet, nbcol)
+larges2<-cbind(larges, lineage3)
 
+surv_mod_larges_lineage2<-glm(larges2$Surv_tplus1 ~ larges2$Diameter_t + larges2$Height_t+larges2$lineage3, family=binomial)
+summary(surv_mod_larges_lineage2)
 
-col = color[facetcol]
+p.vec_E[1]<-surv_mod_larges_lineage2$coeff[1]
+p.vec_E[2]<-surv_mod_larges_lineage2$coeff[2]
+p.vec_E[3]<-surv_mod_larges_lineage2$coeff[3]
 
+p.vec_H[1]<-surv_mod_larges_lineage2$coeff[1] + surv_mod_larges_lineage2$coeff[4]
+p.vec_H[2]<-surv_mod_larges_lineage2$coeff[2]
+p.vec_H[3]<-surv_mod_larges_lineage2$coeff[3]
 
-png(file="D2_surv_overall.png", width=10, height=10, units="in", res=300)
-par(ps=24)
-persp(x1seq_larges, x2seq_larges, z1, ticktype="detailed", theta=-30, zlim=c(0, 1.25), zlab="\n P(Survival)", shade=0.55, nticks=4, xlab="\n Diameter (mm)", ylab="\n \n Height (cm)", col = color[facetcol])
-dev.off()
-
+p.vec_W[1]<-surv_mod_larges_lineage2$coeff[1]
+p.vec_W[2]<-surv_mod_larges_lineage2$coeff[2]
+p.vec_W[3]<-surv_mod_larges_lineage2$coeff[3]
 
 ####GROWTH #####
 
